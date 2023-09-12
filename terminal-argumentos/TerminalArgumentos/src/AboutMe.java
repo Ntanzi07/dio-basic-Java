fdfd18942ede4c84d77a3b70c3d5f1c0ee8a2af7
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2023-09-12 17:01:21 -0300

    AboutMe_2

--- terminal-argumentos/TerminalArgumentos/src/AboutMe.java
+++ terminal-argumentos/TerminalArgumentos/src/AboutMe.java
@@ -1,9 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class AboutMe {
     public static void main(String[] args){
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+
+        System.out.println("Digite seu Nome:");
+        String nome = sc.next();
+
+        System.out.println("Digite seu Sobrenome:");
+        String sobrenome = sc.next();
+
+        System.out.println("Digite sua Idade:");
+        int idade = sc.nextInt();
+
+        System.out.println("Digite sua Altura:");
+        double altura = sc.nextDouble();
+        
 
         System.out.println("Olá me chamo "+ nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos de idade");
